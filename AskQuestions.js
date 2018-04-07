4dbe019e2a4e5de3b5a70cd84997c1c73a8a616b
amiterande <amit.erande@gmail.com>
2018-04-07 09:17:51 -0700

    Create AskQuestions.js

---
+++ AskQuestions.js
@@ -0,0 +1,43 @@
+var questions;
+questions =[];
+var questionobj = function(question, ansarray, correct){
+    this.question = question;
+    this.ansarray = ansarray;
+    this.correct = correct;
+}
+function fillqs(question, answerarray, correct){
+    questions.push(new questionobj(question,answerarray,correct));
+}
+function printrandq(i){    
+    console.log(questions[i].question);
+    for( j=0; j < questions[i].ansarray.length;j++){
+    console.log(questions[i].ansarray[j]);
+    }
+}
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+function checkanswer(resp,i){
+    if ( parseInt(resp) === parseInt(questions[i].correct) ) {
+        return true;
+    } else {
+        return false;
+    }
+}
+fillqs('What is Amit\'s favorite color?', ['Green','Blue','Red'], 0);
+fillqs('What is Amit\'s favorite Car?', ['Nissan','Maruti','Mazda'], 2);
+
+function askquestion() {
+  var random = getRandomInt(2);
+  console.log(random);    
+  printrandq(random);
+  var resp = prompt("Please provide your answer: ");
+  console.log(resp);
+  console.log(questions[random].correct); 
+  if (resp !== 'exit') {
+      console.log(checkanswer(resp,random));
+      askquestion();
+  }    
+}
+
+askquestion(); 
